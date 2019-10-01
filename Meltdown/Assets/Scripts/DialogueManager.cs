887e2651e0f1c17b8350f5f55a4c46c101338e47
Mitchell Faulconbridge <mitchellfaulcon@gmail.com>
2019-10-01 22:14:26 +1300

    Added dialogue system for intro screen #2

---
+++ Meltdown/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Text;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    private Queue<string> sentences;
+
+    public TextMeshProUGUI dialogueText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        StringBuilder sb = new StringBuilder("");
+        dialogueText.text = sb.ToString();
+        foreach(char character in sentence.ToCharArray())
+        {
+            sb.Append(character);
+            dialogueText.text = sb.ToString();
+            yield return null;
+        }
+    }
+
+    private void EndDialogue()
+    {
+        Debug.Log("end of conversation");
+    }
+}
