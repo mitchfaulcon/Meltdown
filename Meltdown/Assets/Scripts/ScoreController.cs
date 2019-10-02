5b838c681dab54275d0769c9a4e403bc0683c1e1
Hayden <hdnsimpson@gmail.com>
2019-10-03 12:24:51 +1300

    Issue #5 - Score bar controller and draining over time

---
+++ Meltdown/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+    {
+
+    public Slider slider;
+    private float currentValue = 0.5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentValue = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentValue -= 0.0003f;
+        slider.value = currentValue;
+    }
+}
