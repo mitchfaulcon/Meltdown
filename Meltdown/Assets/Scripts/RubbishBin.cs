8af8d15d0067563cd403cfe057329218380177ef
Hayden <hdnsimpson@gmail.com>
2019-10-03 10:36:12 +1300

    Issue #5 - Player gets a held item as a result of interactions

--- Meltdown/Assets/Scripts/RubbishBin.cs
+++ Meltdown/Assets/Scripts/RubbishBin.cs
@@ -7,15 +7,16 @@ public class RubbishBin : InteractableObjectBase
     public bool hasRubbish = true;
     public int rubbishLevel = 3;
 
-    public override void OnInteract()
+    public override ItemTypes OnInteract()
     {
         InteractText = "Press J to turn collect rubbish";
         rubbishLevel--;
-        Debug.Log("lowering rubbish level");
+        Debug.Log("lowering rubbish level: " + rubbishLevel);
         if(rubbishLevel == 0)
         {
             hasRubbish = false;
         }
+        return ItemTypes.RubbishBag;
     }
 
     public override bool CanInteract(ItemTypes heldItem)
