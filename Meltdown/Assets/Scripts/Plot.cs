a62dc47ddc1ed1296922a41fedf6fc22e9f16429
Nathan Mills <thenathanmills@gmail.com>
2019-10-03 10:39:27 +1300

    Issue #5 added plot script to interact with plots and plant seeds
    Needs to be attached to plot objects

---
+++ Meltdown/Assets/Scripts/Plot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plot : InteractableObjectBase
+{
+    private bool hasPlant = false;
+    private ItemTypes plotType;
+
+    public override void OnInteract()
+    {
+        InteractText = "Press J to plant Potato seeds";
+        hasPlant = true;
+    }
+
+    public override bool CanInteract(ItemTypes heldItem)
+    {   
+        if(heldItem == plotType)
+        {
+            return !hasPlant;
+        }
+        return false;
+    }
+
+}
