c75e04fdc2ad3153a0848bd8b556b64b5de3b02d
Mitchell Faulconbridge <mitchellfaulcon@gmail.com>
2019-10-05 17:26:09 +1300

    Changed the ingame score meter to a thermometer

--- Meltdown/Assets/Scripts/ScoreController.cs
+++ Meltdown/Assets/Scripts/ScoreController.cs
@@ -8,7 +8,8 @@ public class ScoreController : MonoBehaviour
 
     public Slider slider;
     private float currentValue = 0.5f;
-    
+    public Image barImage;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,13 +19,18 @@ public class ScoreController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        currentValue -= 0.00022f;
+        currentValue += 0.00022f;
+        //Fix value between 0 and 1
+        currentValue = Mathf.Clamp(currentValue, 0f, 1f);
+
         slider.value = currentValue;
+        barImage.fillAmount = currentValue;
+        Debug.Log(currentValue);
     }
 
     public void taskScored(float points)
     {
         Debug.Log("Correct bin, scoring points");
-        currentValue += points;
+        currentValue -= points;
     }
 }
