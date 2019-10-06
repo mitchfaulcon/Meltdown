e4659c68a4cc5c74c8509501f87cb1987436ddf2
Mitchell Faulconbridge <mitchellfaulcon@gmail.com>
2019-10-06 16:54:35 +1300

    Added initial help screen to pause menu (Issue #22)

--- Meltdown/Assets/Scripts/PauseMenu.cs
+++ Meltdown/Assets/Scripts/PauseMenu.cs
@@ -8,6 +8,8 @@ public class PauseMenu : MonoBehaviour
 
     public static bool isPaused = false;
     public GameObject pauseMenuUI;
+    public GameObject pauseMenuCards;
+    public GameObject helpMenuCards;
 
     // Update is called once per frame
     void Update()
@@ -26,26 +28,29 @@ public class PauseMenu : MonoBehaviour
 
     public void Resume()
     {
+        //Disable pause menu UI
         pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
+        pauseMenuCards.SetActive(false);
+        helpMenuCards.SetActive(false);
+
+        Time.timeScale = 1f; //Un-pause time
         isPaused = false;
     }
 
     void Pause()
     {
+        //Enable pause menu UI
         pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
+        pauseMenuCards.SetActive(true);
+        helpMenuCards.SetActive(false);
+
+        Time.timeScale = 0f; //Pause time
         isPaused = true;
     }
 
-    public void ShowHelp()
-    {
-        Debug.Log("Show help menu");
-    }
-
     public void BackToMenu()
-    {
-        Time.timeScale = 1f;
+    { 
+        Time.timeScale = 1f; //Un-pause time
         SceneManager.LoadScene("Main Menu");
     }
 }
