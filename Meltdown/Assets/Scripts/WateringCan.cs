008826dd2d699ee368b7a730ebce6f94fd7af118
Nathan Mills <thenathanmills@gmail.com>
2019-10-06 22:45:28 +1300

    Issue #5 Fixed watering can bug, and task controller bug
    water can now records how many uses it has left, and task list no longer bugs out

--- Meltdown/Assets/Scripts/WateringCan.cs
+++ Meltdown/Assets/Scripts/WateringCan.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class WateringCan : ItemCollectorBase
 {
+    public int waterLevel = 0;
+      public void fill()
+    {
+        waterLevel++;
+        containsItem = true;
+        alert.SetActive(true);
+    }
+
+    public override ItemTypes OnInteract()
+    {
+        waterLevel--;
+        if(waterLevel == 0)
+        {
+            alert.SetActive(false);
+            containsItem = false;
+        }
+        return item;
+    }
 
 }
