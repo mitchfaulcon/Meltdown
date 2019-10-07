5d58b26e8a6192b32d14873bd104d746f0dbb8b8
Eric Pedrido <epedrids@gmail.com>
2019-10-07 20:28:09 +1300

    Increased difficulty
    Decreased how much each task is worth.
    Made a variable rate of increase for the thermometer. Rate increases with each star threshold.

--- Meltdown/Assets/Scripts/ScoreController.cs
+++ Meltdown/Assets/Scripts/ScoreController.cs
@@ -9,20 +9,39 @@ public class ScoreController : MonoBehaviour
     public Slider slider;
     private float currentValue = 1f;
     public Image barImage;
-
+   
+    private static readonly float DEFAULT_RATE = 0.02f;
+    
+    private float increaseRate = DEFAULT_RATE;
     // Update is called once per frame
     void Update()
     {
-        currentValue += 0.022f * Time.deltaTime;
+        currentValue += increaseRate * Time.deltaTime;
         //Fix value between 0 and 1
         currentValue = Mathf.Clamp(currentValue, 0f, 1f);
 
         slider.value = currentValue;
         barImage.fillAmount = currentValue;
+        
+        float newRate = DEFAULT_RATE;
+        if (currentValue > ScoreDisplay.THREESTARTHRESHOLD/1000) {
+            newRate += 0.03f;
+        } else if (currentValue > ScoreDisplay.TWOSTARTHRESHOLD/1000) {
+            newRate += 0.02f;
+        } else if (currentValue > ScoreDisplay.ONESTARTHRESHOLD/1000) {
+            newRate += 0.01f;
+        }
+        increaseRate = newRate;
     }
+
 
     public void taskScored(float points)
     {
         currentValue -= points;
+    }
+
+    public void taskFailed(float points)
+    {
+        currentValue += points;
     }
 }
