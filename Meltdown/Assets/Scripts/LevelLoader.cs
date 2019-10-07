ac0cfa7178670e7eeb4238f3059ec64cd62953cf
Mitchell Faulconbridge <mitchellfaulcon@gmail.com>
2019-10-07 22:07:48 +1300

    Added a loading screen in intro scene for level load progress (Issue #39)

---
+++ Meltdown/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadAsynchronously());
+    }
+
+    // Update is called once per frame
+    IEnumerator LoadAsynchronously()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync("Home_Outdoor");
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            slider.value = progress;
+
+            yield return null;
+        }
+    }
+}
