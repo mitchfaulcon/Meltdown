029862b80578f04ac43a837057b4dabea89a07dc
Mitchell Faulconbridge <mitchellfaulcon@gmail.com>
2019-10-09 16:46:55 +1300

    Added music to main menu (Issue #48)

---
+++ Meltdown/Assets/Scripts/MenuMusicController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusicController : MonoBehaviour
+{
+
+    public AudioSource menuMusic;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (menuMusic.isPlaying == false && GameSettings.music == true)
+        {
+            menuMusic.Play();
+        } else if (menuMusic.isPlaying == true && GameSettings.music == false)
+        {
+            menuMusic.Stop();
+        }
+    }
+}
