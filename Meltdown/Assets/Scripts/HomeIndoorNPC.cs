59a1fcd9c366c171fa3eeb8578543c6575e3657c
Eric Pedrido <epedrids@gmail.com>
2019-10-11 12:13:27 +1300

    Added NPC points
    made all the various routes for the NPC to traverse through. Started on the script.

---
+++ Meltdown/Assets/Scripts/HomeIndoorNPC.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeIndoorNPC : NPCMovement
+{
+
+    private List<Transform[]> routes = new List<Transform[]>();
+    public Transform[] lightSwitchRight;
+    public Transform[] lightSwitchLeft;
+    public Transform[] sink;
+    public Transform[] plateAB;
+    public Transform[] plateCD;
+
+    private Transform prevPoint;
+
+    void Start()
+    {
+        prevPoint = null;
+
+        // Add all routes to list to be randomly selected from
+        routes.Add(lightSwitchRight);
+        routes.Add(lightSwitchLeft);
+        routes.Add(sink);
+        routes.Add(plateAB);
+        routes.Add(plateCD);
+    }
+
+    void Update()
+    {
+        
+    }
+}
