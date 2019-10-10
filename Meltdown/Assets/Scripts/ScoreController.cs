1eb7d00f60d21af9aad064fbcc77dede3afeb6ce
Mitchell Faulconbridge <mitchellfaulcon@gmail.com>
2019-10-11 11:50:03 +1300

    Added tree colour change depending on the temperature (Issue #78)

--- Meltdown/Assets/Scripts/ScoreController.cs
+++ Meltdown/Assets/Scripts/ScoreController.cs
@@ -10,11 +10,21 @@ public class ScoreController : MonoBehaviour
     public Slider slider;
     private float currentValue = 1f;
     public Image barImage;
-   
+
     private static readonly float DEFAULT_RATE = 0.0185f;
     
     private float increaseRate = DEFAULT_RATE;
     public TextMeshProUGUI scoreText;
+
+    public Material treeMaterial;
+    private readonly Color goodTreeColour = new Color(0.8f, 0.8f, 0.8f, 1f); //Normal green colour
+    private readonly Color badTreeColour = new Color(0.635f, 0.318f, 0f, 1f); //Dark brown colour
+    private Color targetColour;
+
+    private void Start()
+    {
+        targetColour = badTreeColour;
+    }
 
     // Update is called once per frame
     void Update()
@@ -37,6 +47,8 @@ public class ScoreController : MonoBehaviour
         increaseRate = newRate;
         Score.GetInstance().SetPoints(currentValue);
         scoreText.text = Score.GetInstance().GetPoints().ToString();
+
+        ColourTrees();
     }
 
 
@@ -48,5 +60,14 @@ public class ScoreController : MonoBehaviour
     public void taskFailed(float points)
     {
         currentValue += points;
+    }
+
+    private void ColourTrees()
+    {
+        //Calculate the new colour as the value of the temperature between the bad & good colours
+        targetColour = (currentValue) * (badTreeColour) + (1 - currentValue) * (goodTreeColour);
+
+        //Smooth the colour change over 2 seconds
+        treeMaterial.color = Color.Lerp(treeMaterial.color, targetColour, Time.deltaTime/2f);
     }
 }
