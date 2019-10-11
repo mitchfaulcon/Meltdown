dd702c4b97fabbb94c8615cd23681d18dfd15ff0
Nathan Mills <thenathanmills@gmail.com>
2019-10-11 15:44:09 +1300

    Issue #92 Added ability to interact with lights and sink

---
+++ Meltdown/Assets/Scripts/Indoor Interactions/IndoorPlayerInteraction.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndoorPlayerInteraction : PlayerInteraction
+{
+    //public GameObject holdingBottle;
+    //public GameObject holdingRubbish;
+    //public GameObject holdingCompost;
+    //public GameObject holdingTomato;
+    //public GameObject holdingTree;
+    //public GameObject holdingPotato;
+    //public GameObject holdingCarrot;
+    //public GameObject holdingCan;
+
+
+    // Remove all item speech bubbles
+    protected override void removeSpeechBubbles()
+    {
+        //holdingBottle.SetActive(false);
+        //holdingRubbish.SetActive(false);
+        //holdingCompost.SetActive(false);
+        //holdingTomato.SetActive(false);
+        //holdingTree.SetActive(false);
+        //holdingPotato.SetActive(false);
+        //holdingCarrot.SetActive(false);
+        //holdingCan.SetActive(false);
+    }
+
+    protected override void setActiveBubble()
+    {
+        //if (heldItem == ItemTypes.Recyclables)
+        //{
+        //    holdingBottle.SetActive(true);
+
+        //}
+        //else if (heldItem == ItemTypes.RubbishBag)
+        //{
+        //    holdingRubbish.SetActive(true);
+        //}
+        //else if (heldItem == ItemTypes.BananaSkin)
+        //{
+        //    holdingCompost.SetActive(true);
+        //}
+        //else if (heldItem == ItemTypes.TomatoSeeds)
+        //{
+        //    holdingTomato.SetActive(true);
+        //}
+        //else if (heldItem == ItemTypes.TreeSappling)
+        //{
+        //    holdingTree.SetActive(true);
+        //}
+        //else if (heldItem == ItemTypes.PotatoSeeds)
+        //{
+        //    holdingPotato.SetActive(true);
+        //}
+        //else if (heldItem == ItemTypes.CarrotSeeds)
+        //{
+        //    holdingCarrot.SetActive(true);
+        //}
+        //else if (heldItem == ItemTypes.WaterBucket)
+        //{
+        //    holdingCan.SetActive(true);
+        //}
+    }
+}
---
+++ Meltdown/Assets/Scripts/Indoor Interactions/ToggleItem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleItem : InteractableObjectBase
+{
+    public bool on;
+    private ItemTypes playerHolding;
+    public bool isLight;
+    public TaskTypes task;
+    public TaskController taskControl;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        taskControl = FindObjectOfType<TaskController>();
+    }
+
+    public override ItemTypes OnInteract()
+    {
+        Debug.Log("Turning off light");
+        on = false;
+        if (isLight)
+        {
+            //code here to disable light in room
+        }
+        else
+        {
+            this.transform.Find("Water").gameObject.SetActive(false);
+            this.transform.Find("SinkWater").gameObject.SetActive(false);
+        }
+        taskControl.taskComplete(task);
+        return playerHolding;
+    }
+
+    // Update is called once per frame
+    public override bool CanInteract(ItemTypes item)
+    {
+        return on;
+    }
+
+    public void fill()
+    {
+        on = true;
+        if(task == TaskTypes.Tap)
+        {
+            this.transform.Find("Water").gameObject.SetActive(true);
+            this.transform.Find("SinkWater").gameObject.SetActive(true);
+        }
+        else
+        {
+            //set light on here
+        }
+    }
+}
