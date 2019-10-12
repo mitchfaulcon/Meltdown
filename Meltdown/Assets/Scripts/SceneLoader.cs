cdee4ad1dbda07f94f52238cd689db92253421d0
salma-s <ssan631@aucklanduni.ac.nz>
2019-10-12 14:52:04 +1300

    Added new scenes to build settings (ISsue #71)

---
+++ Meltdown/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneLoader : MonoBehaviour
+{
+    public string sceneToOpen;
+
+    // The specified scene string to be opened is loaded my the 
+    // scene manager when this method is called
+    public void StartScene()
+    {
+        // Load specified scene
+        SceneManager.LoadScene(sceneToOpen);
+
+    }
+}
