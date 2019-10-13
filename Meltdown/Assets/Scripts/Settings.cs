e0ca3b1fdd68854e9353fcf9b50e90331c8802b9
Mitchell Faulconbridge <mitchellfaulcon@gmail.com>
2019-10-13 15:36:57 +1300

    Added comments

--- Meltdown/Assets/Scripts/Settings.cs
+++ Meltdown/Assets/Scripts/Settings.cs
@@ -18,16 +18,22 @@ public class Settings : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Set toggle states depending on current settings
         soundToggle.isOn = GameSettings.sounds;
         musicToggle.isOn = GameSettings.music;
         fullscreenToggle.isOn = Screen.fullScreen;
+
+        //Populate resolutions dropdown
         SetUpResolutions();
+
+        //Set selected quality to current quality
         qualityDropdown.value = QualitySettings.GetQualityLevel();
         qualityDropdown.RefreshShownValue();
     }
 
     void SetUpResolutions()
     {
+        //Get all available resolutions
         resolutions = Screen.resolutions;
 
         resolutionDropdown.ClearOptions();
@@ -35,10 +41,12 @@ public class Settings : MonoBehaviour
         List<string> options = new List<string>();
 
         int currentResolutionIndex = 0;
+        //Add each resolution to dropdown options
         for (int i = 0; i < resolutions.Length; i++)
         {
             options.Add(resolutions[i].width + " x " + resolutions[i].height);
 
+            //Check if current iteration is the same as current resolution
             if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
             {
                 currentResolutionIndex = i;
@@ -46,14 +54,9 @@ public class Settings : MonoBehaviour
         }
 
         resolutionDropdown.AddOptions(options);
+        //Set selected resolution to current resolution
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     public void ToggleSound(bool soundEnabled)
@@ -73,6 +76,7 @@ public class Settings : MonoBehaviour
 
     public void SetResolution()
     {
+        //Get value of resolution dropdown & set new resolution accordingly
         int resolutionIndex = resolutionDropdown.value;
         Resolution newResolution = resolutions[resolutionIndex];
         Screen.SetResolution(newResolution.width, newResolution.height, Screen.fullScreen);
@@ -80,6 +84,7 @@ public class Settings : MonoBehaviour
 
     public void SetQuality()
     {
+        //Get value of quality dropdown & set new quality accordingly
         int qualityIndex = qualityDropdown.value;
         QualitySettings.SetQualityLevel(qualityIndex);
     }
