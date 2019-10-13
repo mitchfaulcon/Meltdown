86b80118f2daba127d8cdc1152e68d46f9692af1
Mitchell Faulconbridge <mitchellfaulcon@gmail.com>
2019-10-13 15:04:53 +1300

    Added Resolution, Quality, & Fullscreen settings
    Changed settings screen layout aswell

---
+++ Meltdown/Assets/Scripts/Settings.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Settings : MonoBehaviour
+{
+
+    public Toggle soundToggle;
+    public Toggle musicToggle;
+    public Toggle fullscreenToggle;
+    public TMP_Dropdown resolutionDropdown;
+    public TMP_Dropdown qualityDropdown;
+
+    private Resolution[] resolutions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundToggle.isOn = GameSettings.sounds;
+        musicToggle.isOn = GameSettings.music;
+        fullscreenToggle.isOn = Screen.fullScreen;
+        SetUpResolutions();
+        qualityDropdown.value = QualitySettings.GetQualityLevel();
+        qualityDropdown.RefreshShownValue();
+    }
+
+    void SetUpResolutions()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            options.Add(resolutions[i].width + " x " + resolutions[i].height);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleSound(bool soundEnabled)
+    {
+        GameSettings.sounds = soundEnabled;
+    }
+
+    public void ToggleMusic(bool musicEnabled)
+    {
+        GameSettings.music = musicEnabled;
+    }
+
+    public void ToggleFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution()
+    {
+        int resolutionIndex = resolutionDropdown.value;
+        Resolution newResolution = resolutions[resolutionIndex];
+        Screen.SetResolution(newResolution.width, newResolution.height, Screen.fullScreen);
+    }
+
+    public void SetQuality()
+    {
+        int qualityIndex = qualityDropdown.value;
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}
