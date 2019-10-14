0bfff429a6b639cf827a2821ba56e12836939d1e
Hayden <hdnsimpson@gmail.com>
2019-10-14 13:18:28 +1300

    Issue #92 - Created indoor score controller, hooked up to HUD

---
+++ Meltdown/Assets/Scripts/Indoor Interactions/IndoorScoreController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class IndoorScoreController : ScoreController
+{
+    protected new static readonly float DEFAULT_RATE = 0.003f;
+    protected new static readonly float ONE_STAR_RATE = 0.001f;
+    protected new static readonly float TWO_STAR_RATE = 0.003f;
+    protected new static readonly float THREE_STAR_RATE = 0.005f;
+
+    public struct Tasks
+    {
+        public const float SALAD = 0.07f;
+        public const float LIGHT = 0.06f;
+        public const float TAP = 0.06f;
+    }
+
+    private void Start()
+    {
+
+    }
+
+    protected override void updateScenery()
+    {
+
+    }
+}
