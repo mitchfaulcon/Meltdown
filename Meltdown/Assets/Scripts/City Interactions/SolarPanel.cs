188412b158967768dac321766749ecc0d93c57c9
Nathan Mills <thenathanmills@gmail.com>
2019-10-15 22:02:40 +1300

    Issue #109 Made solar panel prefab and script for placing panel

---
+++ Meltdown/Assets/Scripts/City Interactions/SolarPanel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarPanel : InteractableObjectBase
+{
+    private bool solarPanelSet = false;
+    public ResourceCollector toolStore;
+
+    public override bool CanInteract(ItemTypes item)
+    {
+        if(item == ItemTypes.Solar && !solarPanelSet)
+        {
+            return true;
+        }
+        else if(item == ItemTypes.Tools && solarPanelSet)
+        {
+            return true;
+        }
+        return false;
+    }
+    public override ItemTypes OnInteract()
+    {
+        if (!solarPanelSet)
+        {
+            solarPanelSet = true;
+            toolStore.fill();
+            //set panel to fully opaque
+        }
+        else
+        {
+            //have tick appear for task completion
+            this.gameObject.SetActive(false);
+            solarPanelSet = false;
+            //complete solarPanel Tasks
+        }
+    }
+
+    public void setupTask()
+    {
+        //set object back to transparent
+        this.gameObject.SetActive(true);
+    }
+}
