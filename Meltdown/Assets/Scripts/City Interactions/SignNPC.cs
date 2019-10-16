0ebad7270f5cce7a0b1430996f77d7540361382e
salma-s <ssan631@aucklanduni.ac.nz>
2019-10-16 17:18:17 +1300

    City task controller modifications

--- Meltdown/Assets/Scripts/City Interactions/SignNPC.cs
+++ Meltdown/Assets/Scripts/City Interactions/SignNPC.cs
@@ -4,22 +4,33 @@ using UnityEngine;
 
 public class SignNPC : InteractableObjectBase
 {
+    public CityTaskController taskController;
+    public CityScoreController scoreController;
+
     public bool hasSign = false;
     public override bool CanInteract(ItemTypes item)
     {
         if(item == ItemTypes.Sign && !hasSign)
         {
-
             return true;
         }
         return false;
     }
 
+    public void Start()
+    {
+        taskController = GameObject.FindObjectOfType<CityTaskController>();
+        scoreController = GameObject.FindObjectOfType<CityScoreController>();
+    }
+
     public override ItemTypes OnInteract()
     {
-        hasSign = true;
         //make sign visible in NPC hand
+        hasSign = true;
+
         //complete sign task
+        taskController.taskComplete(TaskTypes.Sign);
+    
         return ItemTypes.NONE;
     }
 }
