3c5deae8e112af24abf56ff6c2e82f8507fdf3ff
Brad <bradcoleman1998@gmail.com>
2019-10-16 17:39:29 +1300

    Added pause menu help screen for city

--- Meltdown/Assets/Scripts/Interactions/Outdoor Interactions/OutdoorScoreController.cs
+++ Meltdown/Assets/Scripts/Interactions/Outdoor Interactions/OutdoorScoreController.cs
@@ -31,10 +31,10 @@ public class OutdoorScoreController : ScoreController
 
     protected override void updateScenery()
     {
-        //Calculate the new colour as the value of the temperature between the bad & good colours
-        targetColour = (currentValue) * (badTreeColour) + (1 - currentValue) * (goodTreeColour);
+        ////Calculate the new colour as the value of the temperature between the bad & good colours
+        //targetColour = (currentValue) * (badTreeColour) + (1 - currentValue) * (goodTreeColour);
 
-        //Smooth the colour change over 2 seconds
-        treeMaterial.color = Color.Lerp(treeMaterial.color, targetColour, Time.deltaTime / 2f);
+        ////Smooth the colour change over 2 seconds
+        //treeMaterial.color = Color.Lerp(treeMaterial.color, targetColour, Time.deltaTime / 2f);
     }
 }
