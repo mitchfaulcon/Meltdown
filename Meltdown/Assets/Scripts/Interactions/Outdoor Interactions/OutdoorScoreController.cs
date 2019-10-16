3febc615ebe6b178180ccbebe3a45863378fa465
Mitchell Faulconbridge <mitchellfaulcon@gmail.com>
2019-10-16 22:21:51 +1300

    Added grass changing as temp changes in backyard (Issue #78)

--- Meltdown/Assets/Scripts/Interactions/Outdoor Interactions/OutdoorScoreController.cs
+++ Meltdown/Assets/Scripts/Interactions/Outdoor Interactions/OutdoorScoreController.cs
@@ -21,20 +21,30 @@ public class OutdoorScoreController : ScoreController
     private readonly Color goodTreeColour = new Color(0.8f, 0.8f, 0.8f, 1f); //Normal green colour
     private readonly Color badTreeColour = new Color(0.635f, 0.318f, 0f, 1f); //Dark brown colour
     private Color targetColour;
-    
+
+    public Material grassMaterial;
+    private readonly Color goodGrassColour = new Color(0.561f, 1f, 0.588f, 1f); //Normal green colour
+    private readonly Color badGrassColour = new Color(1f, 0.572f, 0.561f, 1f); //Dark brown colour
+    private Color targetGrassColour;
+
     private void Start()
     {
-        targetColour = badTreeColour;
+        treeMaterial.color = badTreeColour;
+        grassMaterial.color = badGrassColour;
     }
 
     // Update is called once per frame
 
     protected override void updateScenery()
     {
-        ////Calculate the new colour as the value of the temperature between the bad & good colours
+        //Calculate the new colour as the value of the temperature between the bad & good colours
         targetColour = (currentValue) * (badTreeColour) + (1 - currentValue) * (goodTreeColour);
 
-        ////Smooth the colour change over 2 seconds
+        //Smooth the colour change over 2 seconds
         treeMaterial.color = Color.Lerp(treeMaterial.color, targetColour, Time.deltaTime / 2f);
+
+        //Do the same for the grass
+        targetGrassColour = (currentValue) * (badGrassColour) + (1 - currentValue) * (goodGrassColour);
+        grassMaterial.color = Color.Lerp(grassMaterial.color, targetGrassColour, Time.deltaTime / 2f);
     }
 }
