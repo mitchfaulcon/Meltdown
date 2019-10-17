b36596a4b4372486dcb82bbfb9b74d255cd8d702
Hayden <hdnsimpson@gmail.com>
2019-10-17 13:02:17 +1300

    Issue #131 - Slightly tweaked scoring for backyard

--- Meltdown/Assets/Scripts/Interactions/Outdoor Interactions/OutdoorScoreController.cs
+++ Meltdown/Assets/Scripts/Interactions/Outdoor Interactions/OutdoorScoreController.cs
@@ -13,8 +13,8 @@ public class OutdoorScoreController : ScoreController
 
     public struct Tasks
     {
-        public const float SORT_RUBBISH = 0.07f;
-        public const float PLANT = 0.06f;
+        public const float SORT_RUBBISH = 0.08f;
+        public const float PLANT = 0.08f;
     }
 
     public Material treeMaterial;
