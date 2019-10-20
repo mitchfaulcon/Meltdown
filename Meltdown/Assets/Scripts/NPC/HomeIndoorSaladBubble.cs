f77a3c02495c80557a7a902aa2b482c387e2e519
Hayden <hdnsimpson@gmail.com>
2019-10-21 11:53:34 +1300

    Issue #19 - Added comments to the salad bubble script

--- Meltdown/Assets/Scripts/NPC/HomeIndoorSaladBubble.cs
+++ Meltdown/Assets/Scripts/NPC/HomeIndoorSaladBubble.cs
@@ -9,6 +9,7 @@ public class HomeIndoorSaladBubble : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Init the salad bubble rotation toward the camera
         iniRot = transform.rotation;
         iniRot.y = -270;
     }
@@ -16,6 +17,7 @@ public class HomeIndoorSaladBubble : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        // Ensure each frame, the bubble does not rotate
         transform.rotation = iniRot;
     } 
 }
